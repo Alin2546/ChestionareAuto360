8c01a891285053ea1937bc96fc621d783f4d428a
Alin <eternalsq1337@gmail.com>
2025-11-22 15:33:29 +0200

    v0.3 added docker

--- src/main/java/com/chestionare/chestionare360/Chestionare360Application.java
+++ src/main/java/com/chestionare/chestionare360/Chestionare360Application.java
@@ -3,10 +3,13 @@ package com.chestionare.chestionare360;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Chestionare360Application {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Bucharest"));
 		SpringApplication.run(Chestionare360Application.class, args);
 	}
 
