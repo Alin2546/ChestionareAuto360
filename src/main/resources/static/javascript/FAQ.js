992825004b423a01578582574cdba9d493821abb
Alin <eternalsq1337@gmail.com>
2025-11-22 20:10:05 +0200

    added mobile styles

--- src/main/resources/static/javascript/FAQ.js
+++ src/main/resources/static/javascript/FAQ.js
@@ -1,24 +1,24 @@
-  document.addEventListener("DOMContentLoaded", function() {
-      const questions = document.querySelectorAll('.faq-question');
-      questions.forEach(q => {
-          q.addEventListener('click', () => {
-              const answer = q.nextElementSibling;
-              answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
-          });
-      });
+document.addEventListener("DOMContentLoaded", function () {
+    const questions = document.querySelectorAll('.faq-question');
+    questions.forEach(q => {
+        q.addEventListener('click', () => {
+            const answer = q.nextElementSibling;
+            answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
+        });
+    });
 
-      const sidebar = document.getElementById("sidebar");
-      const contentWrapper = document.getElementById("content-wrapper");
-      const menuToggle = document.getElementById("menu-toggle");
-      const closeBtn = document.getElementById("close-btn");
+    const sidebar = document.getElementById("sidebar");
+    const contentWrapper = document.getElementById("content-wrapper");
+    const menuToggle = document.getElementById("menu-toggle");
+    const closeBtn = document.getElementById("close-btn");
 
-      menuToggle.addEventListener("click", () => {
-          sidebar.classList.add("show");
-          contentWrapper.classList.add("shifted");
-      });
+    menuToggle.addEventListener("click", () => {
+        sidebar.classList.add("show");
+        contentWrapper.classList.add("shifted");
+    });
 
-      closeBtn.addEventListener("click", () => {
-          sidebar.classList.remove("show");
-          contentWrapper.classList.remove("shifted");
-      });
-  });
+    closeBtn.addEventListener("click", () => {
+        sidebar.classList.remove("show");
+        contentWrapper.classList.remove("shifted");
+    });
+});
